cde7077152dcbf012be6831beb3a6cae0a8dac4c
gururaj187in <141476751+gururaj187in@users.noreply.github.com>
2023-08-10 22:48:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,5 +62,5 @@ if streamlit.button('Add a fruit to the list'):
   back_from_function=insert_row_snowflake(add_my_fruit)
   streamlit.text(back_from_function)
 streamlit.stop()
-##this will not work
+#this will not work
 
